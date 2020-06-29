20fafde9e1271706b9b5c70332beaaf9581fde41
Jesus Ramirez Vargas <jesus21c.jrv@gmail.com>
2020-06-29 13:36:41 -0500

    necesario

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -22,8 +22,8 @@ style="background-color:#9D9D9C !important;">
         <div class="kt-header__topbar-item kt-header__topbar-item--user">
             <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="0px,0px">
                 <div class="kt-header__topbar-user">
-                    <span class="kt-header__topbar-welcome kt-hidden-mobile">Hi,</span>
-                    <span class="kt-header__topbar-username kt-hidden-mobile">Sean</span>
+                    <span class="kt-header__topbar-welcome kt-hidden-mobile">Hola,</span>
+                    <span class="kt-header__topbar-username kt-hidden-mobile">   {{auth()->user()->name}}</span>
                     <img class="kt-hidden" alt="Pic" src="./assets/media/users/300_25.jpg" />
 
                     <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
@@ -44,7 +44,7 @@ style="background-color:#9D9D9C !important;">
                    
                     </div>
                     <div class="kt-user-card__name">
-                        Sean Stone
+                    {{auth()->user()->name}}
                     </div>
                   
                 </div>
@@ -58,8 +58,14 @@ style="background-color:#9D9D9C !important;">
 
                   
                     <div class="kt-notification__custom kt-space-between">
-                        <a href="demo1/custom/user/login-v2.html" target="_blank"
-                            class="btn btn-label btn-label-brand btn-sm btn-bold">Sign Out</a>
+                    <a style="align-right" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();"
+                                    class="btn btn-clean btn-sm btn-bold">Cerrar sesión
+                                </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
 
                     </div>
                 </div>
